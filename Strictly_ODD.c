b02cddd889b221909e52ac4ac3ebd77cc6d3eee1
Shanoor Jahan Shaik <119839966+Shanoor-jahan@users.noreply.github.com>
2023-01-23 19:29:53 +0530

    #include<stdio.h> int main() {     int n,i,flag;     scanf("%d",&n);     int a[n];     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<n;i++)     {         if(a[i]%2==1)         {             if(i%2==1)             {                 flag=1;             }             else             {                 flag=0;                 break;             }         }     }     if(flag==1)     printf("True");     else     printf("False"); }

--- Strictly_ODD.c
+++ Strictly_ODD.c
@@ -4,27 +4,27 @@ int main()
     int n,i,flag;
     scanf("%d",&n);
     int a[n];
-        for(i=0;i<n;i++)
-        {
+    for(i=0;i<n;i++)
+    {
         scanf("%d",&a[i]);
-        }
-        for(i=0;i<n;i++)
+    }
+    for(i=0;i<n;i++)
+    {
+        if(a[i]%2==1)
         {
-            if(a[i]%2==1)
+            if(i%2==1)
             {
-                if(i%2==1)
-                {
-                    flag=1;
-                }
-                else
-                {
-                    flag=0;
-                    break;
-                }
+                flag=1;
+            }
+            else
+            {
+                flag=0;
+                break;
             }
         }
-        if(flag==1)
-        printf("True");
-        else
-        printf("False");
     }
+    if(flag==1)
+    printf("True");
+    else
+    printf("False");
+}
