29e288fb408ee3da560415f7e421742da9a511b3
Shanoor Jahan Shaik <119839966+Shanoor-jahan@users.noreply.github.com>
2023-05-22 19:58:55 +0530

    #include<stdio.h> int main() {     int a;     scanf("%d",&a);     int r=a%10;     a=a/10;     if(r%2==0 && a%2==0)     printf("Even");     else if(r%2!=0 && a%2!=0)     printf("Odd");     else     printf("Mixed"); }

---
+++ Even_Odd_Mixed.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    scanf("%d",&a);
+    int r=a%10;
+    a=a/10;
+    if(r%2==0 && a%2==0)
+    printf("Even");
+    else if(r%2!=0 && a%2!=0)
+    printf("Odd");
+    else
+    printf("Mixed");
+}
