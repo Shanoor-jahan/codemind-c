f5c33972f63dbe596aebf334a902a7e8c49f2079
Shanoor Jahan Shaik <119839966+Shanoor-jahan@users.noreply.github.com>
2023-05-27 11:24:48 +0530

    #include<stdio.h> int prime(int a) {     int c=0;     for(int i=1;i<=a;i++)     {         if(a%i==0)         {             c++; } } if(c==2) {     return 1;     }     else     {         return 0;         }         }         int main()         {             int n;             scanf("%d" ,&n);             for(int i=1;i<=n;i++)             {                 int a,f,b;                 scanf("%d" ,&a);                 for(int i=a;;i++)                 {                     if(prime(i))                     {                         f=i;                         break;                         }                         }                         for(int i=a;;i--)                         {                             if(prime(i))          {              b=i;              break;          }          }          int d1=f-a;          int d2=a-b;          if(d1>=d2)          {              printf("%d " ,b);              }              else              {                  printf("%d " ,f);              }              }              }

---
+++ Nearest_Prime.c
@@ -0,0 +1,59 @@
+#include<stdio.h>
+int prime(int a)
+{
+    int c=0;
+    for(int i=1;i<=a;i++)
+    {
+        if(a%i==0)
+        {
+            c++;
+}
+}
+if(c==2)
+{
+    return 1;
+    }
+    else
+    {
+        return 0;
+        }
+        }
+        int main()
+        {
+            int n;
+            scanf("%d" ,&n);
+            for(int i=1;i<=n;i++)
+            {
+                int a,f,b;
+                scanf("%d" ,&a);
+                for(int i=a;;i++)
+                {
+                    if(prime(i))
+                    {
+                        f=i;
+                        break;
+                        }
+                        }
+                        for(int i=a;;i--)
+                        {
+                            if(prime(i))
+         {
+             b=i;
+             break; 
+         }
+         }
+         int d1=f-a;
+         int d2=a-b;
+         if(d1>=d2) 
+         {
+             printf("%d
+" ,b);
+             }
+             else 
+             {
+                 printf("%d
+" ,f); 
+             }        
+             }
+             }
+             
